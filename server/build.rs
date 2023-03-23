5074ef3d4fedfac8d3c7c60911eaba8be5afc7a6
Blonteractor <priyanv2003@gmail.com>
2023-03-23 20:23:03 +0530

    Moved protobuf to root

--- server/build.rs
+++ server/build.rs
@@ -1,6 +1,6 @@
 fn main() {
     tonic_build::configure()
         .build_client(false)
-        .compile(&["proto/scheduler/scheduler.proto"], &["proto/scheduler"])
+        .compile(&["../proto/scheduler/scheduler.proto"], &["../proto/scheduler"])
         .unwrap_or_else(|e| panic!("Failed to compile protos: {:?}", e));
 }
