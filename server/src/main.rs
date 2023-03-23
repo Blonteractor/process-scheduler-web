99feedc89b7c812e26daa430e8afb0fb1cb92b90
Blonteractor <priyanv2003@gmail.com>
2023-03-23 13:47:21 +0530

    Implemented service

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use server::{self, scheduler_grpc::scheduler_server::SchedulerServer};
+use tonic::transport::Server;
+
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:10000".parse().unwrap();
+    
+    let scheduler = server::SchedulerService {};
+
+    let svc = SchedulerServer::new(scheduler);
+
+    println!("Server listening on localhost:10000");
+    Server::builder().add_service(svc).serve(addr).await?;
+
+    Ok(())
 }
