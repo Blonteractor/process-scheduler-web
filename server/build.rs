71f3583b122c9ddd45f5ef98dd915ddaa48d4825
Blonteractor <priyanv2003@gmail.com>
2023-03-23 10:30:03 +0530

    Setup build process

---
+++ server/build.rs
@@ -0,0 +1,9 @@
+fn main() {
+    tonic_build::configure()
+        .build_client(false)
+        .compile(
+            &["proto/scheduler/scheduler.proto"],
+            &["proto/scheduler"],
+        ).
+        unwrap_or_else(|e| panic!("Failed to compile protos: {:?}", e));    
+}
