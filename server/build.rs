ada30186ede80842d299ae66ff3bba3f89cedc17
Blonteractor <priyanv2003@gmail.com>
2023-04-06 12:57:14 +0530

    Added dockerfile for server

--- server/build.rs
+++ server/build.rs
@@ -1,6 +1,10 @@
 fn main() {
     tonic_build::configure()
         .build_client(false)
-        .compile(&["../proto/scheduler/scheduler.proto"], &["../proto/scheduler"])
+        .protoc_arg("--experimental_allow_proto3_optional")
+        .compile(
+            &["../proto/scheduler/scheduler.proto"],
+            &["../proto/scheduler"],
+        )
         .unwrap_or_else(|e| panic!("Failed to compile protos: {:?}", e));
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,15 +1,17 @@
+use std::net::SocketAddr;
+
 use server::{self, scheduler_grpc::scheduler_server::SchedulerServer};
 use tonic::transport::Server;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "127.0.0.1:10000".parse().unwrap();
+    let addr: SocketAddr = "127.0.0.1:10000".parse().unwrap();
 
     let scheduler = server::SchedulerService {};
 
     let svc = SchedulerServer::new(scheduler);
 
-    println!("Server listening on localhost:10000");
+    println!("Server listening on {}", addr.to_string());
     Server::builder().add_service(svc).serve(addr).await?;
 
     Ok(())
